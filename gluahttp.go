fc6a3500a34f5ec477a8b0f29d7ebd660b7e3038
lgq12172009 <lgq.1217@163.com>
2018-01-09 16:30:34 +0800

    Add asynchronous features

--- gluahttp.go
+++ gluahttp.go
@@ -29,36 +29,51 @@ func (self *httpModule) Loader(L *lua.LState) int {
 	return 1
 }
 
+func (self *httpModule) AsyncLoader(L *lua.LState) int {
+	mod := L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{
+		"get":     self.asyncGet,
+		"delete":  self.asyncPost,
+		"head":    self.asyncHead,
+		"patch":   self.asyncPatch,
+		"post":    self.asyncPost,
+		"put":     self.asyncPut,
+		"options": self.asyncOptions,
+	})
+	L.Push(mod)
+	return 1
+}
+
+//sync
 func (self *httpModule) get(L *lua.LState) int {
-	return self.doRequestAndPush(L, "GET", L.ToString(1), L.ToTable(2))
+	return self.doRequestAndPush(L, "GET", L.CheckString(1), L.ToTable(2))
 }
 
 func (self *httpModule) delete(L *lua.LState) int {
-	return self.doRequestAndPush(L, "DELETE", L.ToString(1), L.ToTable(2))
+	return self.doRequestAndPush(L, "DELETE", L.CheckString(1), L.ToTable(2))
 }
 
 func (self *httpModule) head(L *lua.LState) int {
-	return self.doRequestAndPush(L, "HEAD", L.ToString(1), L.ToTable(2))
+	return self.doRequestAndPush(L, "HEAD", L.CheckString(1), L.ToTable(2))
 }
 
 func (self *httpModule) patch(L *lua.LState) int {
-	return self.doRequestAndPush(L, "PATCH", L.ToString(1), L.ToTable(2))
+	return self.doRequestAndPush(L, "PATCH", L.CheckString(1), L.ToTable(2))
 }
 
 func (self *httpModule) post(L *lua.LState) int {
-	return self.doRequestAndPush(L, "POST", L.ToString(1), L.ToTable(2))
+	return self.doRequestAndPush(L, "POST", L.CheckString(1), L.ToTable(2))
 }
 
 func (self *httpModule) put(L *lua.LState) int {
-	return self.doRequestAndPush(L, "PUT", L.ToString(1), L.ToTable(2))
+	return self.doRequestAndPush(L, "PUT", L.CheckString(1), L.ToTable(2))
 }
 
 func (self *httpModule) options(L *lua.LState) int {
-	return self.doRequestAndPush(L, "OPTIONS", L.ToString(1), L.ToTable(2))
+	return self.doRequestAndPush(L, "OPTIONS", L.CheckString(1), L.ToTable(2))
 }
 
-func (self *httpModule) doRequestAndPush(L *lua.LState, method string, uri string, options *lua.LTable) int {
-	response, err := self.doRequest(L, method, uri, options)
+func (self *httpModule) doRequestAndPush(L *lua.LState, method string, url string, options *lua.LTable) int {
+	response, err := self.doRequest(L, method, url, options)
 
 	if err != nil {
 		L.Push(lua.LNil)
@@ -68,4 +83,50 @@ func (self *httpModule) doRequestAndPush(L *lua.LState, method string, uri strin
 
 	L.Push(response)
 	return 1
+}
+
+//async
+func (self *httpModule) asyncGet(L *lua.LState) int {
+	return self.asyncDoRequestAndPush(L, "GET", L.CheckString(1), L.ToTable(2))
+}
+
+func (self *httpModule) asyncDelete(L *lua.LState) int {
+	return self.asyncDoRequestAndPush(L, "DELETE", L.CheckString(1), L.ToTable(2))
+}
+
+func (self *httpModule) asyncHead(L *lua.LState) int {
+	return self.asyncDoRequestAndPush(L, "HEAD", L.CheckString(1), L.ToTable(2))
+}
+
+func (self *httpModule) asyncPatch(L *lua.LState) int {
+	return self.asyncDoRequestAndPush(L, "PATCH", L.CheckString(1), L.ToTable(2))
+}
+
+func (self *httpModule) asyncPost(L *lua.LState) int {
+	return self.asyncDoRequestAndPush(L, "POST", L.CheckString(1), L.ToTable(2))
+}
+
+func (self *httpModule) asyncPut(L *lua.LState) int {
+	return self.asyncDoRequestAndPush(L, "PUT", L.CheckString(1), L.ToTable(2))
+}
+
+func (self *httpModule) asyncOptions(L *lua.LState) int {
+	return self.asyncDoRequestAndPush(L, "OPTIONS", L.CheckString(1), L.ToTable(2))
+}
+
+func (self *httpModule) asyncDoRequestAndPush(L *lua.LState, method string, url string, options *lua.LTable) int {
+	resultChan := make(chan lua.LValue, 2)
+
+	go func(L *lua.LState, method string, url string, options *lua.LTable, resultChan chan lua.LValue) {
+		response, err := self.doRequest(L, method, url, options)
+		if err != nil {
+			resultChan <- lua.LNil
+			resultChan <- lua.LString(err.Error())
+		} else {
+			resultChan <- response
+		}
+		close(resultChan)
+	}(L, method, url, options, resultChan)
+
+	return L.Yield(lua.LChannel(resultChan))
 }
